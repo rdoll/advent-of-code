20399ada25c26a78b498add26a9fe390953e9a96
FoamHead <foamhead@gmail.com>
2025-12-13 20:19:53 -0600

    2025 Day 11 Part 2 (try threads)

--- 2025/Day 11/part2.py
+++ 2025/Day 11/part2.py
@@ -5,10 +5,9 @@ import logging
 import queue
 import re
 import sys
-import threading
 from queue import Queue
 from threading import Thread
-from typing import List, Dict, Tuple
+from typing import List, Dict
 
 
 def parse_args() -> argparse.Namespace:
@@ -16,7 +15,7 @@ def parse_args() -> argparse.Namespace:
                                      description='Advent of Code 2025 Day 11 Part 2 Solution')
 
     parser.add_argument('-d', '--debug', action='store_true', help='show debug logs')
-    parser.add_argument('-t', '--threads', metavar='#', type=int, default=1,
+    parser.add_argument('-t', '--threads', metavar='#', type=int, default=2,
                         help='number of concurrent threads')
     parser.add_argument('input_file', metavar="input-file", nargs=1, type=argparse.FileType('r'),
                         help='input file')
@@ -29,7 +28,7 @@ total_paths_checked: int = 0
 devices: Dict[str, List[str]] = {}
 
 pending_queue: Queue[str] = Queue()
-threads: List[Tuple[Thread, int]] = []
+threads: List[List[Thread | int]] = []
 
 def trace_path(path: str) -> int:
     global total_paths_checked
@@ -53,23 +52,23 @@ def trace_path(path: str) -> int:
 
 
 def worker(thread_num: int) -> None:
-    global pending_queue
+    global pending_queue, threads
+    logging.info(f'Started {thread_num=}')
     while True:
         try:
-            path: str = pending_queue.get(block=True, timeout=2)
-            print(f'worker: {path=}')
+            path: str = pending_queue.get(block=True, timeout=0.2)
+            pending_queue.task_done()
             found: int = trace_path(path)
             if found > 0:
-                global threads
                 threads[thread_num][1] += found
-            pending_queue.task_done()
         except queue.Empty:
-            logging.info(f'{threading.current_thread().name} empty queue')
+            logging.info(f'Empty queue, found={threads[thread_num][1]}')
+            return
 
 
 def main() -> int:
     args = parse_args()
-    logging.basicConfig(format='%(asctime)s %(levelname)-7s: %(message)s',
+    logging.basicConfig(format='%(asctime)s %(threadName)-10s %(levelname)-7s: %(message)s',
                         level=logging.DEBUG if args.debug else logging.INFO)
     logging.info(f'Starting {sys.argv[0]} to read {args.input_file[0].name}...')
     logging.debug(f'{args=}')
@@ -85,20 +84,28 @@ def main() -> int:
         devices[device] = connections
     logging.debug(f'Found {len(devices)} {devices=}')
 
-    global threads
-    for thread_num in range(args.threads):
-        thread: Thread = Thread(target=trace_path, name=f'Thread-{thread_num:02}', args=[thread_num])
-        threads.append((thread, 0))
-        thread.start()
+    global pending_queue, threads
+    try:
+        for thread_num in range(args.threads):
+            thread: Thread = Thread(target=worker, name=f'Thread-{thread_num:02}', args=(thread_num,))
+            threads.append([thread, 0])
+            thread.start()
 
-    global pending_queue
-    pending_queue.put('svr')
-    pending_queue.join()
-    logging.info(f'Queue empty and terminated...')
+        pending_queue.put('svr')
+        pending_queue.join()
+        logging.info(f'Queue empty and terminated...')
+    except Exception as e:
+        logging.error(f'Exception encountered: {e}')
+        pending_queue.shutdown(immediate=True)
+        sys.exit(-1)
 
-    total: int = sum([t[1] for t in threads])
-    logging.info(f'Final total: {total=}')
+    logging.info(f'Waiting for threads to finish...')
+    for t in threads:
+        t[0].join()
     logging.debug(f'{total_paths_checked=:,}')
+
+    total: int = sum([int(t[1]) for t in threads])
+    logging.info(f'Final {total=}')
 
     logging.info('Completed successfully')
     return 0
