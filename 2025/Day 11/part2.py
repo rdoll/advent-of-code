67807b96d2d6029586f24d1fd781889ff69e601a
FoamHead <foamhead@gmail.com>
2025-12-14 03:04:34 -0600

    2025 Day 11 Part 2 WIP

--- 2025/Day 11/part2.py
+++ 2025/Day 11/part2.py
@@ -15,7 +15,7 @@ def parse_args() -> argparse.Namespace:
                                      description='Advent of Code 2025 Day 11 Part 2 Solution')
 
     parser.add_argument('-d', '--debug', action='store_true', help='show debug logs')
-    parser.add_argument('-p', '--processes', metavar='#', type=int, default=1,
+    parser.add_argument('-p', '--processes', metavar='#', type=int, default=2,
                         help='number of concurrent processes')
     parser.add_argument('input_file', metavar="input-file", nargs=1, type=argparse.FileType('r'),
                         help='input file')
@@ -24,57 +24,81 @@ def parse_args() -> argparse.Namespace:
     return args
 
 
-total_paths_checked: ctypes.c_int = mp.Value(ctypes.c_int, 0)
-pending_queue: mp.JoinableQueue = mp.JoinableQueue()
-processes: List[mp.Process] = []
-process_results: List[ctypes.c_int]
+class Worker:
+    work_queue: mp.JoinableQueue
+    results: ctypes.c_int
+    devices: Dict[str, List[str]]
+    paths_checked: int
+    found: int
+    max_depth: int
 
-def trace_path(path: str,
-               devices: Dict[str, List[str]],
-               path_queue: mp.JoinableQueue,
-               paths_checked: ctypes.c_int) -> int:
-    logger = mp.get_logger()
+    def __init__(self,
+                 work_queue: mp.JoinableQueue,
+                 results: ctypes.c_int,
+                 devices: Dict[str, List[str]]) -> None:
+        self.work_queue = work_queue
+        self.results = results
+        self.devices = devices
+        self.paths_checked = 0
+        self.found = 0
+        self.max_depth = 0
 
-    pc: int = paths_checked.value + 1
-    paths_checked.value = pc
-    if pc % 1_000_000 == 0:
-        logger.info(f'{pc=:,}')
+    def __repr__(self):
+        return (f'Worker(results.value={self.results.value}, #devices={len(self.devices)}'
+            + f', paths_checked={self.paths_checked}, found={self.found}, max_depth={self.max_depth})')
 
-    device: str = path[-3:]
-    if device == 'out':
-        if 'dac' in path and 'fft' in path:
-            logger.info(f'Found {path=}')
-            return 1
-        return 0
+    def trace_path(self, path: str, depth: int) -> int:
+        if depth > self.max_depth:
+            self.max_depth = depth
 
-    for connection in devices[device]:
-        if not connection in path:
-            path_queue.put(path + ' ' + connection)
-    return 0
+        self.paths_checked += 1
+        if self.paths_checked % 500_000 == 0:
+            mp.get_logger().info(f'{self.paths_checked=:,}, {self.max_depth=}')
+
+        device: str = path[-3:]
+        if device == 'out':
+            if 'dac' in path and 'fft' in path:
+                mp.get_logger().info(f'Found {path=}')
+                return 1
+            return 0
+
+        found: int = 0
+        for connection in self.devices[device]:
+            if not connection in path:
+                next_path: str = f'{path} {connection}'
+                if depth % 5 == 0:
+                    self.work_queue.put(next_path)
+                else:
+                    found += self.trace_path(next_path, depth + 1)
+        return found
+
+    def start(self) -> None:
+        mp.get_logger().info(f'Starting {self}')
+
+        try:
+            while True:
+                path: str = self.work_queue.get(block=True, timeout=0.5)
+                self.work_queue.task_done()
+                depth: int =  ((len(path) - 3) // 4) + 1
+                # mp.get_logger().info(f'Dequeued {path=}, {depth=}')
+                found: int = self.trace_path(path, depth)
+                if found > 0:
+                    self.found += found
+                    mp.get_logger().info(f'Found {found=}, {self.found=}')
+        except queue.Empty:
+            self.results.value = self.found
+            mp.get_logger().info(f'Empty queue, final state {self}')
 
 
-def worker(process_num: int,
-           total_found: ctypes.c_int,
-           devices: Dict[str, List[str]],
-           path_queue: mp.JoinableQueue,
-           paths_checked: ctypes.c_int) -> None:
-    logger = mp.get_logger()
-    for handler in mp.get_logger().handlers:
+def configure_logger_format(logger: logging.Logger) -> None:
+    for handler in logger.handlers:
         handler.formatter = logging.Formatter('%(asctime)s %(processName)-11s %(levelname)-7s: %(message)s')
-    logger.info(f'Started {process_num=}, {total_found=}, {len(devices)=}, {path_queue=}')
 
-    try:
-        while True:
-            path: str = path_queue.get(block=True, timeout=0.5)
-            path_queue.task_done()
-            # logger.info(f'{path=}')
-            found: int = trace_path(path, devices, path_queue, paths_checked)
-            if found > 0:
-                logger.info(f'Found {found=}')
-                total_found.value += found
-    except queue.Empty:
-        logger.info(f'Empty queue, {total_found=}')
-        return
+
+def start_worker(work_queue: mp.JoinableQueue, results: ctypes.c_int, devices: Dict[str, List[str]]) -> None:
+    configure_logger_format(mp.get_logger())
+    worker: Worker = Worker(work_queue, results, devices)
+    worker.start()
 
 
 def main() -> int:
@@ -95,30 +119,30 @@ def main() -> int:
         devices[device] = connections
     logging.debug(f'Found {len(devices)} {devices=}')
 
-    global pending_queue, processes, process_results
     mp.log_to_stderr(logging.INFO)
-    process_results = [mp.Value(ctypes.c_int, 0) for _ in range(args.processes)]
+    configure_logger_format(mp.get_logger())
+
+    work_queue: mp.JoinableQueue = mp.JoinableQueue()
+    processes: List[mp.Process] = []
+    process_results: List[ctypes.c_int] = [mp.Value(ctypes.c_int, 0) for _ in range(args.processes)]
     try:
+        work_queue.put('svr')
         for process_num in range(args.processes):
-            process: mp.Process = mp.Process(
-                target=worker,
-                name=f'Process-{process_num:02}',
-                args=(process_num, process_results[process_num], devices, pending_queue, total_paths_checked))
+            process: mp.Process = mp.Process(target=start_worker,
+                                             name=f'Process-{process_num:02}',
+                                             args=(work_queue, process_results[process_num], devices))
             processes.append(process)
             process.start()
-
-        pending_queue.put('svr', block=True, timeout=None)
-        pending_queue.join()
-        logging.info(f'Queue empty and terminated...')
     except Exception as e:
         logging.error(f'Exception encountered: {e}')
-        pending_queue.close()
+        work_queue.close()
+        for process in processes:
+            process.close()
         sys.exit(-1)
 
     logging.info(f'Waiting for {args.processes} processes to finish...')
     for p in processes:
         p.join()
-    logging.debug(f'{total_paths_checked.value=:,}')
 
     total: int = sum([p.value for p in process_results])
     logging.info(f'Final {total=}')
